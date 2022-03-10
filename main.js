ae6b5371f096078125d40bcd259289d68b6b5d89
LuchiAdaobi <Luchichronicles@gmail.com>
2022-03-10 17:49:17 +0100

    added UI class

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ class UI {
         isbn: '28467294',
       },
       {
-        title: 'Book One',
+        title: 'Book Two',
         author: 'John Jones',
         isbn: '28467294',
       },
@@ -29,17 +29,34 @@ class UI {
 
     const books = StoredBooks;
 
-    books.forEach((book) => {
-      UI.addBookToList(book);
-    });
+    books.forEach((book) => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href ="#" class =" btn btn-danger btn-sm delete">X</a></td>
+    <td><i class="fa-regular fa-heart fav"></i></td>
+    <td><i class="fa-solid fa-heart toggle"></i></td>
+    `;
+
+    list.appendChild(row);
+  }
+
+  static deleteBook() {
+
   }
 }
 
+// EVENT HANDLERS
+
 // EVENT: Display Book
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // EVENT: ADD BOOK
 
